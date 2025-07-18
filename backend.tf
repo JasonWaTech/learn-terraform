16efe5ac29225a557fd248f7c2001c5f58cd16ae
JasonWaTech <jason64364.work@gmail.com>
2025-07-18 16:05:30 -0700

    JN 1605

--- backend.tf
+++ backend.tf
@@ -1,10 +1,11 @@
 
-backend "azurerm" {
+terraform {
+  backend "azurerm" {
     resource_group_name   = "WaTech-AID-AISandboxRG"
     storage_account_name  = "j64364terraformstatesa"
     container_name        = "tfstate"
     key                   = "jason-terraform.tfstate"
+  }
 }
-
 
 
