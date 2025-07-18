eccc2244a7613baff09dba35c423e2855ea06380
JasonWaTech <jason64364.work@gmail.com>
2025-07-18 15:50:37 -0700

    JN 1550

--- main.tf
+++ main.tf
@@ -9,6 +9,15 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  features {}
+
+  subscription_id = "af9f2529-62ee-4b3f-a212-2083e1c96470" 
+  tenant_id       = "11d0e217-264e-400a-8ba0-57dcc127d72d"
+  alias           = "watech"
+  default_tags = local.default_tags 
+}
+
 
 #
 # Resources go here
